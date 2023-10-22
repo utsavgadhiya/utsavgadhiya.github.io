82e5abf7315ddba13954ee7d5011155431827313
Utsav Gadhiya <utsavpatel3913@gmail.com>
2023-10-22 18:30:54 -0400

    Add new code for portfolio site

---
+++ src/pages/content.js
@@ -0,0 +1,154 @@
+// Personal Information --> #hero section
+export const name = "Utsav Gadhiya"
+export const designation = "Full Stack JavaScript Developer"
+export const location = "Toronto, Canada"
+
+// About
+export const about = "Experienced Software Developer with a proven track record of 2+ years in the industry. Specializing in designing and developing APIs and microservices, I am deeply passionate about backend development, cloud technologies, and DevOps practices."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "Aug 2023",
+		to: "Oct 2023",
+		role: "Web Developer Intern",
+		org: "Just Natural Paws",
+		org_url: "https://www.justnaturalpaws.org/",
+		location: "Toronto, Canada"
+	},
+	{
+		from: "May 2023",
+		to: "Aug 2023",
+		role: "Software Developer Intern",
+		org: "The Transmogrify",
+		org_url: "https://www.thetransmogrify.com/",
+		location: "Toronto, Canada"
+	},
+	{
+		from: "Dec 2019",
+		to: "Nov 2021",
+		role: "Software Developer",
+		org: "SRKay Consulting Group",
+		org_url: "https://www.srkay.com/",
+		location: "Surat, India"
+	}
+]
+
+export const skills = [
+	{
+		skill: "JavaScript",
+		icon: "logos:javascript"
+	},
+	{
+		skill: "TypeScript",
+		icon: "logos:typescript-icon"
+	},
+	{
+		skill: "Node.js",
+		icon: "logos:nodejs-icon"
+	},
+	{
+		skill: "React.js",
+		icon: "logos:react"
+	},
+	{
+		skill: "Angular",
+		icon: "logos:angular-icon"
+	},
+	{
+		skill: "Python",
+		icon: "logos:python"
+	},
+	{
+		skill: "Java",
+		icon: "logos:java"
+	},
+	{
+		skill: "C#",
+		icon: "vscode-icons:file-type-csharp2"
+	},
+	{
+		skill: "HTML5",
+		icon: "vscode-icons:file-type-html"
+	},
+	{
+		skill: "CSS3",
+		icon: "vscode-icons:file-type-css"
+	},
+	{
+		skill: "Django",
+		icon: "akar-icons:django-fill"
+	},
+	{
+		skill: "Flask",
+		icon: "simple-icons:flask"
+	},
+	{
+		skill: "Postgres",
+		icon: "logos:postgresql"
+	},
+	{
+		skill: "MongoDB",
+		icon: "vscode-icons:file-type-mongo"
+	},
+	{
+		skill: "Redis",
+		icon: "logos:redis"
+	},
+	{
+		skill: "GraphQL",
+		icon: "logos:graphql"
+	},
+	{
+		skill: "Docker",
+		icon: "logos:docker-icon"
+	},
+	{
+		skill: "Jenkins",
+		icon: "vscode-icons:file-type-jenkins"
+	},
+	{
+		skill: "Git",
+		icon: "logos:git-icon"
+	},
+	{
+		skill: "AWS",
+		icon: "logos:aws"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "BookHub",
+		description: "An online books webapp created with React, Node, MongoDB.",
+		url: "https://github.com/utsavgadhiya/BookHub",
+		tech: "JavaScript(Node.js), React, MongoDB",
+	},
+	{
+		title: "ToDoNow",
+		description: "Easy-to-use to-do app for effective task management and organization.",
+		url: "https://github.com/utsavgadhiya/ToDoNow",
+		tech: "React, Tailwind CSS",
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2022",
+		to: "2023",
+		institute: "Lambton College",
+		course: "Post Grad in Computer Software and Database Development",
+		url: "https://lambtoncollege.ca/",
+		location: "Toronto, Canada"
+	},
+	{
+		from: "2016",
+		to: "2020",
+		institute: "Ganpat University",
+		course: "Bachelor of Technology in Information Technology",
+		url: "https://www.ganpatuniversity.ac.in/",
+		location: "Mehsana, India"
+	}
+]
