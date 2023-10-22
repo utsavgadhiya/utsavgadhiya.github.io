b99acd9c2368cf2e94d673388a39e07bafbd6e03
Utsav Gadhiya <utsavpatel3913@gmail.com>
2023-10-22 19:38:50 -0400

    Remove unwanted dependency and update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
   output: 'dist',
-  adapter: vercel({
-    analytics: true,
-  }),
+  publicDir: 'dist'
   site: 'https://utsavgadhiya.github.io'
 });
